62514aa242c89c522c762b6fb37a723c494b07ac
Pavel <pavel-zh@inbox.ru>
2022-02-22 14:30:56 +0300

    corrected model

--- todo/mainapp/admin.py
+++ todo/mainapp/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
-
+from mainapp.models import AppUsers
 # Register your models here.
+
+# Красивый вывод данных в таблице
+class AppUsersAdmin(admin.ModelAdmin):
+    list_display = ('uid', 'username', 'email', 'first_name', 'last_name', 'birthday', 'is_active', 'add_datetime')
+
+
+admin.site.register(AppUsers, AppUsersAdmin)
--- todo/mainapp/models.py
+++ todo/mainapp/models.py
@@ -1,12 +1,18 @@
+from django.contrib import admin
+from datetime import datetime
 from django.db import models
+from django.conf import settings
 from uuid import uuid4
 # Create your models here.
 
 
-class User(models.Model):
+class AppUsers(models.Model):
     uid = models.UUIDField(primary_key=True, default=uuid4(), unique=True)
     username = models.CharField(verbose_name='логин пользователя',  max_length=64, unique=True, null=False)
     email = models.EmailField(verbose_name='email пользователя', null=False, unique=True)
     first_name = models.CharField(verbose_name='имя пользователя', max_length=64, null=False)
     last_name = models.CharField(verbose_name='фамилия пользователя', max_length=64, null=True)
-    birthday_year = models.PositiveIntegerField()
+    birthday = models.DateField(verbose_name='дата рождения пользоватя', null=False)
+    is_active = models.BooleanField(verbose_name="активный пользователь", default=True)
+    add_datetime = models.DateTimeField(verbose_name='дата и время создания', auto_now_add=True)
+    last_modified = models.DateTimeField(verbose_name='последнее изменение', auto_now=True)
