bd36d4fb404d514752043e1e464a5ff8807c1fd5
Pavel <pavel-zh@inbox.ru>
2022-02-22 17:06:18 +0300

    add command for users

--- todo/mainapp/models.py
+++ todo/mainapp/models.py
@@ -7,12 +7,12 @@ from uuid import uuid4
 
 
 class AppUsers(models.Model):
-    uid = models.UUIDField(primary_key=True, default=uuid4(), unique=True)
+    uid = models.UUIDField(primary_key=True, default=uuid4(), null=False, unique=True)
     username = models.CharField(verbose_name='логин пользователя',  max_length=64, unique=True, null=False)
     email = models.EmailField(verbose_name='email пользователя', null=False, unique=True)
     first_name = models.CharField(verbose_name='имя пользователя', max_length=64, null=False)
     last_name = models.CharField(verbose_name='фамилия пользователя', max_length=64, null=True)
-    birthday = models.DateField(verbose_name='дата рождения пользоватя', null=False)
+    birthday = models.DateField(verbose_name='дата рождения пользоватя')
     is_active = models.BooleanField(verbose_name="активный пользователь", default=True)
     add_datetime = models.DateTimeField(verbose_name='дата и время создания', auto_now_add=True)
     last_modified = models.DateTimeField(verbose_name='последнее изменение', auto_now=True)
--- todo/mainapp/serializers.py
+++ todo/mainapp/serializers.py
@@ -5,4 +5,5 @@ from mainapp.models import AppUsers
 class AppUsersSerializer(HyperlinkedModelSerializer):
     class Meta:
         model = AppUsers
-        fields = '__all__'
+        # fields = '__all__'
+        fields = ['username', 'first_name', 'last_name', 'email']
