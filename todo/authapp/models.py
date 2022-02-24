62b7ee2309bba1a1ead1a892b60864b6f18cb6c7
Pavel <pavel-zh@inbox.ru>
2022-02-24 09:33:56 +0300

    Add authapp

---
+++ todo/authapp/models.py
@@ -0,0 +1,11 @@
+from django.db import models
+from django.contrib.auth.models import AbstractUser
+from uuid import uuid4
+
+
+class ApiUser(AbstractUser):
+    uid = models.UUIDField(primary_key=True, default=uuid4(), null=False, unique=True)
+    add_datetime = models.DateTimeField(verbose_name='дата и время создания', auto_now_add=True)
+    last_modified = models.DateTimeField(verbose_name='последнее изменение', auto_now=True)
+
+# Create your models here.
