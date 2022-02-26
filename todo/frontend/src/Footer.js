85a35bf6677de231257ebf1089b071f871b05e29
Pavel <pavel-zh@inbox.ru>
2022-02-26 16:38:15 +0300

    Tasks for Lesson-2

--- todo/frontend/src/Footer.js
+++ todo/frontend/src/Footer.js
@@ -5,13 +5,13 @@ const Footer = () => {
     <div class="container">
         <footer class="py-3 my-4">
             <ul class="nav justify-content-center border-bottom pb-3 mb-3">
-                <li class="nav-item"><a href="#" class="nav-link px-2 text-muted">Home</a></li>
-                <li class="nav-item"><a href="#" class="nav-link px-2 text-muted">Features</a></li>
-                <li class="nav-item"><a href="#" class="nav-link px-2 text-muted">Pricing</a></li>
-                <li class="nav-item"><a href="#" class="nav-link px-2 text-muted">FAQs</a></li>
-                <li class="nav-item"><a href="#" class="nav-link px-2 text-muted">About</a></li>
+                <li class="nav-item"><a href="#" class="nav-link px-2 text-muted">Главная</a></li>
+                <li class="nav-item"><a href="#" class="nav-link px-2 text-muted">Решения</a></li>
+                <li class="nav-item"><a href="#" class="nav-link px-2 text-muted">Цены</a></li>
+                <li class="nav-item"><a href="#" class="nav-link px-2 text-muted">Документация</a></li>
+                <li class="nav-item"><a href="#" class="nav-link px-2 text-muted">О нас</a></li>
             </ul>
-            <p class="text-center text-muted">&copy; 2021 Company, Inc</p>
+            <p class="text-center text-muted">&copy; 2022 ToDo Service, Inc</p>
         </footer>
     </div>
     )
--- todo/frontend/src/Menu.js
+++ todo/frontend/src/Menu.js
@@ -4,7 +4,7 @@ const Menu = () => {
     return (
         <nav class="navbar navbar-expand navbar-dark bg-dark" aria-label="Second navbar example">
             <div class="container-fluid">
-                <a class="navbar-brand" href="#">Library</a>
+                <a class="navbar-brand" href="#">ToDo Users</a>
                 <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarsExample02" aria-controls="navbarsExample02" aria-expanded="false" aria-label="Toggle navigation">
                     <span class="navbar-toggler-icon"></span>
                 </button>
